fc27a19db63619e9387dd32f77be3592ce035fe0
Potwora <matthewpotwora@yahoo.com>
2022-06-11 21:11:41 -0700

    worked on css styling for a bit

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// var getWeatherRepos = function() {
+//     console.log("function was called");
+//   };
+  
+//   getWeatherRepos();
+let weather = {
+    "apiKey": "e35f214583eabb86c9a7ac5f2334a45b"
+}
+  
